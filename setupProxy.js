3566395cd79633ba6abd56a2ef4e1d3ce1a59500
Maurice Jules M <julesmaurice.mulisa@gmail.com>
2023-08-29 10:39:28 +0200

    added proxy setup  to use live dhis2 instances

---
+++ setupProxy.js
@@ -0,0 +1,39 @@
+const express = require("express");
+let cors = require("cors");
+
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+let sessionCookie = "";
+const onProxyReq = (proxyReq) => {
+    if (sessionCookie) {
+        proxyReq.setHeader("cookie", sessionCookie);
+    }
+};
+const onProxyRes = (proxyRes) => {
+    const proxyCookie = proxyRes.headers["set-cookie"];
+    if (proxyCookie) {
+        sessionCookie = proxyCookie;
+    }
+};
+// proxy middleware options
+const options = {
+    target: "https://aggregate.moh.gov.rw/hmis",
+    onProxyReq,
+    onProxyRes,
+    changeOrigin: true, // needed for virtual hosted sites
+    auth: undefined,
+    logLevel: "debug",
+};
+
+// create the proxy (without context)
+const exampleProxy = createProxyMiddleware(options);
+
+const app = express();
+app.use(
+    cors({
+        credentials: true,
+        origin: ["http://localhost:3000"],
+    })
+);
+app.use("/", exampleProxy);
+app.listen(3002);
